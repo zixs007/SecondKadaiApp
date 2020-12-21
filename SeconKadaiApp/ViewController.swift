09a425ae0c81362439f8fd24f528d769cdc2b4a9
yuto <yuto00711127@ezweb.ne.jp>
2020-12-21 17:05:29 +0900

    Initial Commit

---
+++ SeconKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeconKadaiApp
+//
+//  Created by 河崎優人 on 2020/12/21.
+//  Copyright © 2020 yuto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
